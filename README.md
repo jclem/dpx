953632fd716817885e10b07b2d6bef3ae7f91aa1
Jonathan Clem <jonathan@jclem.net>
2020-10-02 15:43:10 -0400

    Support workflow dispatch events

--- README.md
+++ README.md
@@ -1,30 +1,36 @@
 # dpx <img src="send.svg" align="bottom" height="24px" />
 
-This is a utility for sending [repository dispatch][dispatch] events to
-GitHub repositories.
+This is a utility for sending [repository dispatch][repository_dispatch] and
+[workflow dispatch][workflow_dispatch] events to GitHub repositories.
 
 ## Use
 
-This tool attempts to allow you to dispatch events quickly with as little typing as possible, provided you've got your environment set up for it:
+This tool attempts to allow you to dispatch events quickly with as little
+typing as possible, provided you've got your environment set up for it:
 
 - You can avoid passing a GitHub token manually by setting a `GITHUB_TOKEN`
   environment variable.
 - You can avoid passing a GitHub repository by having a git remote called
   "origin" in your .git/config that points to a GitHub repository
 - By default, dpx attempts to parse values as JSON (pass `--raw` to avoid this)
+- The presence of the `-w/--workflow` flag indicates dpx should send a
+  workflow dispatch event instead of a repository dispatch event.
 
 ```shell
-# Send a "test" event.
+# Send a "test" repo dispatch event.
 > npx dpx test
 
-# Send a "deploy" event with a payload `{"branch": "master"}`.
+# Send a "deploy" repo dispatch event with a payload `{"branch": "master"}`.
 > npx dpx deploy branch=master
 
-# Send a "db-migrate" event with a payload `{"direction": "up", "count": 1}`
+# Send a "db-migrate" repo dispatch event with a payload `{"direction": "up", "count": 1}`
 > npx dpx db-migrate direction=up count=1
 
-# Send a "db-migrate" event with a payload `{"direction": "up", "count": "1"}`
+# Send a "db-migrate" repo dispatch event with a payload `{"direction": "up", "count": "1"}`
 > npx dpx --raw db-migrate direction=up count=1
+
+# Trigger the "test.yml" workflow on the "dev" branch with the payload `{"fast": "true"}`
+> npx dpx -w test.yml -f dev fast=true
 ```
 
 ### Flags & Arguments
@@ -36,7 +42,9 @@ This tool attempts to allow you to dispatch events quickly with as little typing
 - `--raw` Do not parse values in key=value pairs
 - `-d`/`--dry-run` Log event type and payload, but don't send a request
 - `-h`/`--help` Display the help message
-- `$event` The first non-flag argument is interpreted as the event
+- `-w`/`--workflow` The workflow to use for a workflow dispatch event
+- `-f`/`--ref` The ref to run from for a workflow dispatch event (defaults to the default branch of the repository)
+- `$event` The first non-flag argument is interpreted as the event for repository dispatches
 - `$key=$value` Any number of key-value pairs that are parsed and sent as a
   JSON object as the event payload
 
@@ -44,11 +52,13 @@ This tool attempts to allow you to dispatch events quickly with as little typing
 
 In the GitHub ecosystem, a repository dispatch event is an event sent to a
 particular GitHub repository by a user, app, or tool with an arbitrary type
-and JSON payload.
+and JSON payload. A workflow dispatch is an event that triggers a single
+workflow with a predefined set of inputs.
 
 Since GitHub Actions allows workflows to be kicked off by these events, this
-flexibility makes repository dispatches a powerful tool for automating many
-day-to-day async tasks, such as app deployments, building and packaging, and
-many other use cases.
+flexibility makes repository and workflow dispatches a powerful tool for
+automating many day-to-day async tasks, such as app deployments, building and
+packaging, and many other use cases.
 
-[dispatch]: https://developer.github.com/v3/repos/#create-a-repository-dispatch-event
+[repository_dispatch]: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#create-a-repository-dispatch-event
+[workflow_dispatch]: https://docs.github.com/en/free-pro-team@latest/rest/reference/actions#create-a-workflow-dispatch-event
--- index.js
+++ index.js
@@ -8,114 +8,190 @@ const https = require('https')
 const path = require('path')
 const args = require('minimist')(process.argv.slice(2), {
   boolean: ['d', 'dry-run', 'h', 'help', 'raw', 'v', 'version'],
-  string: ['r', 'repo', 't', 'token'],
+  string: ['r', 'repo', 't', 'token', 'w', 'workflow', 'f', 'ref']
 })
 const parseKV = require('./parse-kv')
 
 const sshPattern = /^git@github.com:(.+)\.git$/
 const httpsPattern = /^https:\/\/github\.com\/(.+)$/
-const dryRun = args.d || args['dry-run']
 
-if (args.v || args.version) {
-  console.log(require('./package.json').version)
-  process.exit(0)
-}
-
-if (args.h || args.help) {
-  printHelp()
-  process.exit(0)
-}
-
-let nwo = args.r || args.repo
-const [event, ...kvPairs] = args._
-
-if (!nwo) {
-  try {
-    const config = ini.parse(
-      fs.readFileSync(path.join(process.cwd(), '.git', 'config')).toString()
-    )
-
-    const remote = config['remote "origin"'].url
-    const match = remote.match(sshPattern) || remote.match(httpsPattern)
-    if (match) nwo = match[1]
-    if (nwo.endsWith('.git')) nwo = nwo.slice(0, nwo.length - 4)
-  } catch (err) {
-    // Just ignore this, we'll error out in the assert.
-  }
-}
-
-if (!dryRun && !nwo) {
-  console.error('Must provide a repo or have one as "origin" in .git/config')
-  printHelp()
-  process.exit(1)
-}
-
-if (!event) {
-  console.error('Must provide an event as the first positional argument')
-  printHelp()
-  process.exit(1)
-}
-
-const payload = {event_type: event}
-
-if (kvPairs.length) {
-  payload.client_payload = kvPairs.reduce(
-    (clientPayload, kv) =>
-      Object.assign(clientPayload, parseKV(kv, {raw: args.raw})),
-    {}
-  )
-}
-
-if (dryRun) {
-  console.log(JSON.stringify(payload, null, 2))
-  process.exit(0)
-} else {
-  const req = https.request(
-    `https://api.github.com/repos/${nwo}/dispatches`,
-    {
-      method: 'POST',
-      headers: {
-        authorization: `Bearer ${
-          args.t || args.token || process.env.GITHUB_TOKEN
-        }`,
-        'user-agent': 'npm.im/dpx',
-        'content-type': 'application/json',
-      },
-    },
-    (res) => {
-      if (res.statusCode === 204) {
-        process.exit(0)
-      } else {
-        console.error(`Got unexpected status code ${res.statusCode}`)
-        process.exit(1)
-      }
-    }
-  )
-
-  req.on('error', (err) => {
+main()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch(err => {
     console.error(err)
     process.exit(1)
   })
 
-  req.write(JSON.stringify(payload))
-  req.end()
+async function main() {
+  const dryRun = args.d || args['dry-run']
+
+  if (args.v || args.version) {
+    console.log(require('./package.json').version)
+    process.exit(0)
+  }
+
+  if (args.h || args.help) {
+    printHelp()
+    process.exit(0)
+  }
+
+  let nwo = args.r || args.repo
+
+  if (!nwo) {
+    try {
+      const config = ini.parse(
+        fs.readFileSync(path.join(process.cwd(), '.git', 'config')).toString()
+      )
+
+      const remote = config['remote "origin"'].url
+      const match = remote.match(sshPattern) || remote.match(httpsPattern)
+      if (match) nwo = match[1]
+      if (nwo.endsWith('.git')) nwo = nwo.slice(0, nwo.length - 4)
+    } catch (err) {
+      // Just ignore this, we'll error out in the assert.
+    }
+  }
+
+  if (!dryRun && !nwo) {
+    console.error('Must provide a repo or have one as "origin" in .git/config')
+    printHelp()
+    process.exit(1)
+  }
+
+  const workflow = args.w || args.workflow
+  let ref = args.f || args.ref
+
+  if (workflow && !ref) {
+    const {body} = await request('GET', `/repos/${nwo}`)
+    ref = body.default_branch
+
+    if (!ref) {
+      console.error('Must provide a ref if given a workflow')
+      printHelp()
+      process.exit(1)
+    }
+  }
+
+  if (ref && !workflow) {
+    console.error('Must provide a workflow if given a ref')
+    printHelp()
+    process.exit(1)
+  }
+
+  let event, kvPairs
+  if (workflow) {
+    kvPairs = args._
+  } else {
+    ;[event, ...kvPairs] = args._
+  }
+
+  if (!workflow && !event) {
+    console.error(
+      'Must provide an event as the first positional argument when making a repository dispatch'
+    )
+    printHelp()
+    process.exit(1)
+  }
+
+  const payload = event ? {event_type: event} : {ref}
+
+  if (kvPairs.length) {
+    payload[event ? 'client_payload' : 'inputs'] = kvPairs.reduce(
+      (clientPayload, kv) =>
+        Object.assign(clientPayload, parseKV(kv, {raw: args.raw})),
+      {}
+    )
+  }
+
+  const url = event
+    ? `/repos/${nwo}/dispatches`
+    : `/repos/${nwo}/actions/workflows/${workflow}/dispatches`
+
+  if (dryRun) {
+    console.log(url)
+    console.log(JSON.stringify(payload, null, 2))
+    process.exit(0)
+  } else {
+    const {res, body} = await request('POST', url, payload)
+
+    if (res.statusCode === 204) {
+      process.exit(0)
+    } else {
+      console.error(`Got unexpected status code ${res.statusCode}`)
+      console.error(body)
+      process.exit(1)
+    }
+  }
+}
+
+function request(method, url, payload) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(
+      `https://api.github.com${url}`,
+      {
+        method,
+        headers: {
+          accept: 'application/vnd.github.v3+json',
+          authorization: `Bearer ${
+            args.t || args.token || process.env.GITHUB_TOKEN
+          }`,
+          'user-agent': 'npm.im/dpx',
+          'content-type': 'application/json'
+        }
+      },
+      res => {
+        const data = []
+        res.on('data', ch => data.push(ch))
+        res.on('end', () => {
+          resolve({
+            res,
+            body:
+              res.statusCode === 204
+                ? ''
+                : JSON.parse(Buffer.concat(data).toString('utf-8'))
+          })
+        })
+      }
+    )
+
+    req.on('error', err => {
+      reject(err)
+    })
+
+    if (payload) req.write(JSON.stringify(payload))
+
+    req.end()
+  })
 }
 
 function printHelp() {
   console.log(`Send a repository dispatch event to a GitHub repository.
-  
+
 Usage:
   dpx [...flags] [event] [...key=value]
 
 Examples:
-  dpx deploy branch=master
-  dpx -r jclem/dpx -t $githubtoken deploy branch=master
-  
+
+  Create a repository dispatch:
+
+    dpx deploy branch=master
+    dpx --repo=jclem/dpx --token=$githubtoken deploy branch=master
+
+  Create a workflow dispatch:
+
+    dpx --workflow=test
+
 Flags:
-  -r, --repo    The repository (e.g. jclem/dpx) to dispatch to
-  -t, --token   A GitHub personal access token with repo scope
-  -v, --version Display the version of dpx
-      --raw     Do not parse key=value pair values
-  -d, --dry-run Log the event type and payload, but do not send a request
-  -h, --help    Display this message`)
+  -r, --repo     The repository (e.g. jclem/dpx) to dispatch to
+  -t, --token    A GitHub personal access token with repo scope
+  -v, --version  Display the version of dpx
+      --raw      Do not parse key=value pair values
+  -d, --dry-run  Log the event type and payload, but do not send a request
+  -h, --help     Display this message
+
+Flags for workflow dispatch only:
+  -w, --workflow A workflow ID to dispatch to
+  -f, --ref      The Git ref to run the workflow from (uses the default branch if not provided)`)
 }
